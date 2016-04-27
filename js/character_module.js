32c1909d838fdc5b2679ad0156c03a2420848e0c
brendanerquiaga <brendanerquiaga@gmail.com>
2016-04-27 12:50:23 -0700

    Character Data Loading from 'json'

--- js/character_module.js
+++ js/character_module.js
@@ -1,25 +1,62 @@
-var potraitLocation = "assets/portraits/"
+var potraitLocation = "assets/portraits/";
 
-function initHeroData(){
-  $('.character-name').text(hero1.Name);
+// function initHeroData(){
+//   $('.character-name').text(hero1.Name);
+//
+//   var portraitSource = potraitLocation + hero1.Name + ".jpg";
+//
+//   $('.character-card .avatar img').attr('src', portraitSource);
+// }
 
-  var portraitSource = potraitLocation + hero1.Name + ".jpg";
+function setupCharacter(characterID) {
+  var characterIDString = "#character-id-" + characterID;
 
-  $('.character-card .avatar img').attr('src', portraitSource);
+  console.log(characterIDString);
+
+  var portraitSource = potraitLocation + characters[i].name + ".jpg";
+
+  $(characterIDString).find('.avatar img').attr('src', portraitSource);
+  $(characterIDString).find('.card-up').removeClass('indigo').addClass(getClassColor(characters[i].class));
+  $(characterIDString).find(".character-name").text(characters[i].name);
+  $(characterIDString).find(".character-class").text(characters[i].domain + " " + characters[i].class);
+  $(characterIDString).find(".character-description").text(characters[i].description);
+};
+
+function getClassColor(characterClass) {
+  switch (characterClass) {
+    case "Bard":
+      return 'pink';
+    case "Barbarian":
+      return 'red darken-4';
+    case "Cleric":
+      return 'yellow accent-4';
+    case "Druid":
+      return 'teal darken-2';
+    case "Fighter":
+      return 'grey darken-2';
+    case "Paladin":
+      return 'amber darken-4 ';
+    case "Ranger":
+      return 'light-green darken-4';
+    case "Rogue":
+      return 'black';
+    case "Sorcerer":
+      return 'light-blue darken-4';
+    case "Wizard":
+      return 'blue darken-1';
+    case "Warlock":
+      return 'purple darken-4';
+    default:
+      return 'indigo';
+  };
+};
+
+function setupCharacterCards(){
+  for(i = 0; i < characters.length; i++){
+    setupCharacter(i);
+  }
 }
-
-var hero1Data;
-
-function load(){
-  //var actual_JSON = $.getJSON("/../assets/test.json");
-  //console.log(actual_JSON);
-
-  console.log(hero1);
-}
-
-
 
 $(document).ready(function() {
-    load();
-    initHeroData();
+    setupCharacterCards();
 });
