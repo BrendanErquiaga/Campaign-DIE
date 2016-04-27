1b9331285aec6e893a304636c5fd087843d1ba9e
brendanerquiaga <brendanerquiaga@gmail.com>
2016-04-27 12:51:37 -0700

    Characters Updated

---
+++ assets/characters.js
@@ -0,0 +1,142 @@
+var Drax =
+{
+  "id" : 0,
+  "name" : "Drax",
+  "class" : "Barbarian",
+  "domain" : "Totem",
+  "description" : "Sturdy, Relentless, Angry. Perfect for players who want to charge headlong into battle without worrying about dying. "
+};
+
+var Rask =
+{
+  "id" : 1,
+  "name" : "Rask",
+  "class" : "Paladin",
+  "domain" : "Ancients",
+  "description" : "Holy Defender of the realm. A great choice for players who want to be self sufficient while protecting their allies."
+};
+
+var Groob =
+{
+  "id" : 2,
+  "name" : "Groob",
+  "class" : "Fighter",
+  "domain" : "Eldritch Knight",
+  "description" : "A tricky fighter who uses magic. A good choice for the player who wants it all and isn't afraid of complex decisions. "
+};
+
+var Galrak =
+{
+  "id" : 3,
+  "name" : "Sir Galrak",
+  "class" : "Paladin",
+  "domain" : "Devotion",
+  "description" : "Protection with a concious. Good for those who wish to protect their allies with divine magic."
+};
+
+var Melianthe =
+{
+  "id" : 4,
+  "name" : "Melianthe",
+  "class" : "Druid",
+  "domain" : "Grassland Circle",
+  "description" : "A nature oriented spell caster. Perfect for the hippy who wants to save their friends. "
+};
+
+var Craef =
+{
+  "id" : 5,
+  "name" : "Craef",
+  "class" : "Cleric",
+  "domain" : "Life",
+  "description" : "The archtypical healer. Seriously if you want to heal play this."
+};
+
+var Thokk =
+{
+  "id" : 6,
+  "name" : "Thokk",
+  "class" : "Barbarian",
+  "domain" : "Berserker",
+  "description" : "RAAAAAGE! If you want to hit stuff hard..."
+};
+
+var Dala =
+{
+  "id" : 7,
+  "name" : "Dala",
+  "class" : "Ranger",
+  "domain" : "Hunter",
+  "description" : "The archer. No seriously she shoots stuff. "
+};
+
+var Vala =
+{
+  "id" : 8,
+  "name" : "Vala",
+  "class" : "Rogue",
+  "domain" : "Assassin",
+  "description" : "Stabby Ms. Stabberson. "
+};
+
+var Loopmottin =
+{
+  "id" : 9,
+  "name" : "Loopmottin",
+  "class" : "Wizard",
+  "domain" : "Evocation",
+  "description" : "PEW PEW PEW. Magic Missile! Probably other things but mostly magic missle..."
+};
+
+var Arumi =
+{
+  "id" : 10,
+  "name" : "Arumi",
+  "class" : "Sorcerer",
+  "domain" : "Red Draconic",
+  "description" : "Did you want to cast a fireball? Like a lot? Here you go."
+};
+
+var Tish =
+{
+  "id" : 11,
+  "name" : "Tish",
+  "class" : "Warlock",
+  "domain" : "Archfey Pact",
+  "description" : "For weirdos & players who want to beguile their enemies. "
+};
+
+var Gleoc =
+{
+  "id" : 12,
+  "name" : "Gleoc",
+  "class" : "Bard",
+  "domain" : "Lore",
+  "description" : "Not Kvothe but like Kvothe."
+};
+
+var Ulfgar =
+{
+  "id" : 13,
+  "name" : "Ulfgar",
+  "class" : "Cleric",
+  "domain" : "Trickery",
+  "description" : "Support character with an emphasis on deception. "
+};
+
+var Elorfil =
+{
+  "id" : 14,
+  "name" : "Elorfil",
+  "class" : "Druid",
+  "domain" : "Arctic Circle",
+  "description" : "Slow your enemies down & Let It Go. "
+};
+
+var characters = [
+  Drax, Rask, Groob,
+  Galrak, Melianthe, Craef,
+  Thokk, Dala, Vala,
+  Loopmottin, Arumi, Tish,
+  Gleoc, Ulfgar,Elorfil
+];
