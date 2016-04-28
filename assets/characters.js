6ca7148388b956c3f73b6393ac7815fcf2b7b3fb
Brendan Erquiaga <brendanerquiaga@gmail.com>
2016-04-27 22:00:26 -0700

    Initial Launch (#1)
    * MDB & Initial Splash
    
    * Started Characters Page
    
    * Character Page!
    
    * Character Data Loading from 'json'
    
    * Characters Updated
    
    * Character Page Complete
    
    Added About Page
    
    Fixed Header & Removed Footer
    
    * Fixed Error
    
    * Copy Changes...
    
    * Navbar Style
    
    * Officers & Play With Us Section
    
    * Final Layout?
    
    * Updating Character Names
    
    * Final Touches

---
+++ assets/characters.js
@@ -0,0 +1,157 @@
+var Drax =
+{
+  "id" : 0,
+  "name" : "Drax",
+  "class" : "Barbarian",
+  "domain" : "Totem",
+  "description" : "Perfect for players who want to charge headlong into battle without worrying about dying. ",
+  "link" : "https://drive.google.com/file/d/0BwGDzNUXpcCEbDMwQXB2V2V5bjA/view"
+};
+
+var Rask =
+{
+  "id" : 1,
+  "name" : "Rask",
+  "class" : "Paladin",
+  "domain" : "Ancients",
+  "description" : "A great choice for players who want to be self sufficient while protecting their allies.",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEYXJUOHpIWllIZWc"
+};
+
+var Groob =
+{
+  "id" : 2,
+  "name" : "Groob",
+  "class" : "Fighter",
+  "domain" : "Eldritch Knight",
+  "description" : "Good for the player who wants to fight & use magic. Complex but rewarding.",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEOF9vdnJQU0ZzMlk"
+};
+
+var Galrak =
+{
+  "id" : 3,
+  "name" : "Sir Galrak",
+  "class" : "Paladin",
+  "domain" : "Devotion",
+  "description" : "Protection with a concious. Good for those who wish to protect their allies with divine magic.",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEVkF6RE8tcEpDQzQ"
+};
+
+var Melianthe =
+{
+  "id" : 4,
+  "name" : "Meli",
+  "class" : "Druid",
+  "domain" : "Grassland",
+  "description" : "A nature oriented spell caster. Perfect for the hippy who wants to save their friends. ",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCENHpBRGNWc3cyMFU"
+};
+
+var Craef =
+{
+  "id" : 5,
+  "name" : "Craef",
+  "class" : "Cleric",
+  "domain" : "Life",
+  "description" : "Seriously if you want to heal...",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEVTFWVEtqTm5aams"
+};
+
+var Thokk =
+{
+  "id" : 6,
+  "name" : "Thokk",
+  "class" : "Barbarian",
+  "domain" : "Berserker",
+  "description" : "RAAAAAGE! If you want to hit stuff...",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEYXh3alhRM1U2bU0"
+};
+
+var Dala =
+{
+  "id" : 7,
+  "name" : "Dala",
+  "class" : "Ranger",
+  "domain" : "Hunter",
+  "description" : "The archer. Seriously she shoots stuff. ",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEWlZ5MU5MalZHVkE"
+};
+
+var Vala =
+{
+  "id" : 8,
+  "name" : "Vala",
+  "class" : "Rogue",
+  "domain" : "Assassin",
+  "description" : "Stabby Ms. Stabberson. Stabby!",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEQ2oyX3JuMl9lSTg"
+};
+
+var Loopmottin =
+{
+  "id" : 9,
+  "name" : "Loop",
+  "class" : "Wizard",
+  "domain" : "Evocation",
+  "description" : "PEW PEW PEW. Magic Missile!",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEWGFMeWV6WmJNUjQ"
+};
+
+var Arumi =
+{
+  "id" : 10,
+  "name" : "Arumi",
+  "class" : "Sorcerer",
+  "domain" : "Draconic",
+  "description" : "Cast fireball! Cast it a lot!!!",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEaFk5d2VJQU5zeUE"
+};
+
+var Tish =
+{
+  "id" : 11,
+  "name" : "Tish",
+  "class" : "Warlock",
+  "domain" : "Archfey",
+  "description" : "Beguile your enemies.",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEanZsMGpwc0QwYTQ"
+};
+
+var Gleoc =
+{
+  "id" : 12,
+  "name" : "Gleoc",
+  "class" : "Bard",
+  "domain" : "Lore",
+  "description" : "Not Kvothe but like Kvothe.",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEejh2aGIxTnJaRmM"
+};
+
+var Ulfgar =
+{
+  "id" : 13,
+  "name" : "Ulfgar",
+  "class" : "Cleric",
+  "domain" : "Trickery",
+  "description" : "Deception based support character. ",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEVlpNREt4LVIxc0E"
+};
+
+var Elorfil =
+{
+  "id" : 14,
+  "name" : "Elorfil",
+  "class" : "Druid",
+  "domain" : "Arctic",
+  "description" : "Slow your enemies down & Let It Go. ",
+  "link" : "https://drive.google.com/open?id=0BwGDzNUXpcCEZ0JDZWRTVW1nWlE"
+};
+
+var characters = [
+  Drax, Rask, Groob,
+  Galrak, Melianthe, Craef,
+  Thokk, Dala, Vala,
+  Loopmottin, Arumi, Tish,
+  Gleoc, Ulfgar,Elorfil
+];
---
+++ js/character_module.js
@@ -0,0 +1,61 @@
+var potraitLocation = "assets/portraits/";
+
+// function initHeroData(){
+//   $('.character-name').text(hero1.Name);
+//
+//   var portraitSource = potraitLocation + hero1.Name + ".jpg";
+//
+//   $('.character-card .avatar img').attr('src', portraitSource);
+// }
+
+function setupCharacter(characterID) {
+  var characterIDString = "#character-id-" + characterID;
+  
+  var portraitSource = potraitLocation + characters[i].name + ".jpg";
+
+  $(characterIDString).find('.avatar img').attr('src', portraitSource);
+  $(characterIDString).find('.card-up').removeClass('indigo').addClass(getClassColor(characters[i].class));
+  $(characterIDString).find(".character-name").text(characters[i].name);
+  $(characterIDString).find(".character-class").text(characters[i].domain + " " + characters[i].class);
+  $(characterIDString).find(".character-description").text(characters[i].description);
+  $(characterIDString).find(".character-sheet").attr('href', characters[i].link);
+};
+
+function getClassColor(characterClass) {
+  switch (characterClass) {
+    case "Bard":
+      return 'pink';
+    case "Barbarian":
+      return 'red darken-4';
+    case "Cleric":
+      return 'yellow accent-4';
+    case "Druid":
+      return 'teal darken-2';
+    case "Fighter":
+      return 'grey darken-2';
+    case "Paladin":
+      return 'amber darken-4 ';
+    case "Ranger":
+      return 'light-green darken-4';
+    case "Rogue":
+      return 'black';
+    case "Sorcerer":
+      return 'light-blue darken-4';
+    case "Wizard":
+      return 'blue darken-1';
+    case "Warlock":
+      return 'purple darken-4';
+    default:
+      return 'indigo';
+  };
+};
+
+function setupCharacterCards(){
+  for(i = 0; i < characters.length; i++){
+    setupCharacter(i);
+  }
+}
+
+$(document).ready(function() {
+    setupCharacterCards();
+});
